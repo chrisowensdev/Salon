d73aff9437bd1e844a770484b1977adb31b6fc01
Katy Sage <katysagehj@gmail.com>
2020-09-24 15:51:03 -0400

    added fetch of image info and edited image page

--- routes/image.js
+++ routes/image.js
@@ -2,10 +2,12 @@
 
 const express = require('express'),
     db = require('../models/conn'),
+    fetch = require('node-fetch'),
     router = express.Router();
 
 const reviewsList = require('../models/reviewsModel'),
     favoritesList = require('../models/favoritesModel');
+var bigData;
 
 
 //GET render and data for images searched
@@ -23,11 +25,25 @@ router.get('/', async (req, res) => {
 //GET all reviews for this object_id
 router.get('/:object_id?', async (req, res) => {
     const objectReviews = await reviewsList.showAllReviewsObject(req.params.object_id);
-    console.log(objectReviews);
+    const objId = req.params.object_id
+    fetch(`https://api.artic.edu/api/v1/artworks/${objId}`)
+        .then(res => res.json())
+        .then(json => 
+            { bigData = {
+            id : json.data.id,
+                title: json.data.title,
+                url: json.data.thumbnail.url,
+                artist_display: json.data.artist_display,
+                description: json.data.description
+        }
+    
+})
+console.log(bigData)
     res.render('template', {
         locals: {
             title: '',
             data: objectReviews,
+            imgData: bigData,
             is_logged_in: req.session.is_logged_in,
         },
         partials: {
@@ -49,6 +65,7 @@ router.post('/:object_id?', async (req, res) => {
 });
 //DELETE review at specific review.id
 router.delete('/:object_id?', async (req, res) => {
+    const object_id = req.params.object_id
     if (req.body.delete === 'delete') {
         const {
             id
