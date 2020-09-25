bc2243a8979759a596b91e9f3a99b3adc3b054b1
Chris Owens <guitarrockstarco@gmail.com>
2020-09-25 16:00:46 -0400

    Search

---
+++ routes/search.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express'),
+    db = require('../models/conn'),
+    fetch = require('node-fetch'),
+    router = express.Router();
+
+//GET render and data for images searched
+router.post('/', async (req, res) => {
+    res.render('template', {
+        locals: {
+            title: 'Search',
+            searchTerm: req.body.searchTerm,
+            is_logged_in: req.session.is_logged_in,
+        },
+        partials: {
+            partial: 'partial-image',
+        },
+    });
+});
+
+module.exports = router;
