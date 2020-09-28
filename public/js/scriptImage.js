ea609872cf22922ea1000c96c521b211d945909a
Justin Gardner <the1jscott@gmail.com>
2020-09-28 10:12:52 -0500

    search results/pagination small cleanup

--- public/js/scriptImage.js
+++ public/js/scriptImage.js
@@ -1,13 +1,10 @@
 'use strict';
-'use strict';
 
 const imageDiv = document.querySelector('.images');
 // const searchTerm = document.getElementById('searchTerm').value;
 let number = document.getElementById('page_number').value
-if (number === 1){
-    let searchTerm = document.getElementById('searchTerm').value;
-    localStorage.setItem("searchTerm", searchTerm)
-}
+let searchTerm = document.getElementById('searchTerm').value
+console.log(number)
 
 const search = async (searchTerm, number) => {
     let searchUrl = `https://api.artic.edu/api/v1/artworks/search?q=${searchTerm}&page=${number}`;
@@ -26,9 +23,33 @@ const getObject = async (objID) => {
 
 (async function () {
     // search() returns a promise, so we need to `await` it
+    if (number === "1"){
+        console.log(searchTerm)
+        localStorage.setItem("searchTerm", searchTerm)
+        const searchData = await search(searchTerm, number);  
+        let objectArray = [];
+    searchData.data.map(async (objID) => {
+        objectArray.push(objID.id);
+    });
+    console.log(objectArray);
+    objectArray.map(async (object) => {
+        const objectData = await getObject(object);
+        console.log(object);
+        const smallImage = objectData.data.thumbnail.url;
+        console.log(smallImage);
+        const url = smallImage + '/full/full/0/default.jpg';
+        const link = document.createElement('a');
+        link.setAttribute('href', `/image/${object}`);
+        const image = document.createElement('img');
+        image.setAttribute('src', url);
+        image.className += 'thumbnail';
+        imageDiv.appendChild(link);
+        link.appendChild(image);
+    });
+} else {
     let searchTerm = localStorage.getItem("searchTerm")
-    const searchData = await search(searchTerm, number);    
-    console.log(localStorage.getItem("searchTerm"));
+    console.log(searchTerm)
+    const searchData = await search(searchTerm, number);
     let objectArray = [];
     searchData.data.map(async (objID) => {
         objectArray.push(objID.id);
@@ -48,5 +69,5 @@ const getObject = async (objID) => {
         imageDiv.appendChild(link);
         link.appendChild(image);
     });
-
+}
 })();
